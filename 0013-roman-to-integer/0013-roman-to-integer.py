72fffb7fd4c10a6e92caa82ff7d0dc74c6bc7558
Uppada Giridhar Karthik <121222746+Karthik94598@users.noreply.github.com>
2024-08-20 11:00:15 +0530

    Time: 44 ms (70.68%), Space: 16.5 MB (43.50%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -9,4 +9,39 @@ class Solution:
             else:
                 num += ri[s[i]]
         return num
-        
+# Iteration Over Each Character:
+
+# Index 0 (M):
+
+# Value is 1000.
+# Next numeral is C (value 100).
+# Since 1000 > 100, add 1000 to num. Now, num = 1000.
+# Index 1 (C):
+
+# Value is 100.
+# Next numeral is M (value 1000).
+# Since 100 < 1000, subtract 100 from num. Now, num = 900.
+# Index 2 (M):
+
+# Value is 1000.
+# Next numeral is X (value 10).
+# Since 1000 > 10, add 1000 to num. Now, num = 1900.
+# Index 3 (X):
+
+# Value is 10.
+# Next numeral is C (value 100).
+# Since 10 < 100, subtract 10 from num. Now, num = 1890.
+# Index 4 (C):
+
+# Value is 100.
+# Next numeral is I (value 1).
+# Since 100 > 1, add 100 to num. Now, num = 1990.
+# Index 5 (I):
+
+# Value is 1.
+# Next numeral is V (value 5).
+# Since 1 < 5, subtract 1 from num. Now, num = 1989.
+# Index 6 (V):
+
+# Value is 5.
+# There’s no next numeral, so add 5 to num. Now, num = 1994.
