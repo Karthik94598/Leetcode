da1922313616cebdb22851d5dd66f4c341c5e84f
Uppada Giridhar Karthik <121222746+Karthik94598@users.noreply.github.com>
2024-08-28 09:22:36 +0530

    Time: 43 ms (16.52%), Space: 16.4 MB (67.46%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,10 +1,8 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        while True:
-            if val in nums:
-                nums.remove(val)
-            else:
-                break
-        k=len(nums)
+        k = 0  # Pointer to track the position of the non-val elements
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
         return k
-        
