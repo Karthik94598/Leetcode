68006669f254ac608a5f97293661527d9635e4de
Uppada Giridhar Karthik <121222746+Karthik94598@users.noreply.github.com>
2024-09-03 06:36:35 +0530

    Time: 40 ms (55.76%), Space: 16.5 MB (66.00%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,10 +1,8 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
         if x < 2:
-            return x
-        
+            return x        
         left, right = 2, x // 2
-
         while left <= right:
             mid = (left + right) // 2
             num = mid * mid
@@ -14,6 +12,5 @@ class Solution:
             elif num < x:
                 left = mid + 1
             else:
-                right = mid - 1
-        
+                right = mid - 1        
         return right
